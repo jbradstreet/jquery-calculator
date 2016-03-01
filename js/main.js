4a1ec158c9f38bd421cee3053550d7f6ed5a3409
Jenni Bradstreet <jennic15@gmail.com>
2016-02-29 21:42:57 -0800

    Store more than button click at a time to the screen

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 
 
   $(document).ready(function() {
-
+    var number = '';
     $('span').click(function(event) {
-
-      // store the span text output into a variable
       var $spanOutput = $(this).text();
-      console.log($spanOutput);
+      number = number + $spanOutput;
+      console.log(number);
 
-      // get the <div id='screen'> as a target
       var $screenOutput = $('#screen');
-      // use spanOutput as '.text' in <div id='screen'>
-      $screenOutput.text($spanOutput);
+      $screenOutput.text(number);
 
     });
 
