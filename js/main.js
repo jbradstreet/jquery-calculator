9a0e251d19ad219232c24b0b96b784f322558576
Jenni Bradstreet <jennic15@gmail.com>
2016-02-29 21:29:42 -0800

    Add code so buttons pressed goes into the screen

--- js/main.js
+++ js/main.js
@@ -3,7 +3,16 @@
   $(document).ready(function() {
 
     $('span').click(function(event) {
-      console.log($(this).text());
+
+      // store the span text output into a variable
+      var $spanOutput = $(this).text();
+      console.log($spanOutput);
+
+      // get the <div id='screen'> as a target
+      var $screenOutput = $('#screen');
+      // use spanOutput as '.text' in <div id='screen'>
+      $screenOutput.text($spanOutput);
+
     });
 
 
