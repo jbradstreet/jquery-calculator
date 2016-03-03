4c84c93f9b8497bbfb15a9db09440a8e52e9ebbb
Jenni Bradstreet <jennic15@gmail.com>
2016-03-03 15:23:02 -0800

    Make calculator multiply!

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
   $(document).ready(function() {
     var number = '';
+
+
     $('span').click(function(event) {
       var $spanOutput = $(this).text();
       number = number + $spanOutput;
@@ -11,6 +13,20 @@
       $screenOutput.text(number);
 
     });
+
+    $('#calc').click(function(event) {
+      var doTheMath = number.split('x');
+        console.log(parseInt(doTheMath[0]) * parseInt(doTheMath[1]));
+
+
+    });
+
+    // need to hide the "C" somehow...
+    // also need to make this work with one click
+
+    // $('#cancel').click(function(event) {
+    //   number = '';
+    // });
 
 
   });
