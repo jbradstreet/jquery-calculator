249a42b10c7f20f74b839dc1a6a13881a66e91f2
Jenni Bradstreet <jennic15@gmail.com>
2016-03-03 15:53:13 -0800

    Store operators into a variable

--- js/main.js
+++ js/main.js
@@ -1,32 +1,65 @@
 
 
   $(document).ready(function() {
-    var number = '';
+    var input = '';
+    var operator = '';
 
 
     $('span').click(function(event) {
       var $spanOutput = $(this).text();
-      number = number + $spanOutput;
-      console.log(number);
+      input = input + $spanOutput;
+      console.log(input);
 
       var $screenOutput = $('#screen');
-      $screenOutput.text(number);
+      $screenOutput.text(input);
+
+      if (input === 'x') {
+        operator = 'x';
+      } else {
+        operator = 'x';
+      }
 
     });
 
     $('#calc').click(function(event) {
-      var doTheMath = number.split('x');
+      var doTheMath = input.split(operator);
         console.log(parseInt(doTheMath[0]) * parseInt(doTheMath[1]));
-
-
     });
 
     // need to hide the "C" somehow...
     // also need to make this work with one click
 
-    // $('#cancel').click(function(event) {
-    //   number = '';
-    // });
-
+    $('#cancel').click(function(event) {
+      input = '';
+    });
 
   });
+
+
+  // attempt to split number inputs as 'left' and 'right'
+
+  // $(document).ready(function() {
+  //   var number = '';
+  //   var left = '';
+  //   var right = '';
+  //
+  //
+  //   $('span:not(.operator)').click(function(event) {
+  //     var $spanOutput = $(this).text();
+  //     number = number + $spanOutput;
+  //     console.log(number);
+  //
+  //     var $screenOutput = $('#screen');
+  //     $screenOutput.text(number);
+  //
+  //   });
+  //
+  //   $('.operator').click(function(event) {
+  //     number = left;
+  //     console.log(left);
+  //       $('span:not(.operator)').click(function(event) {
+  //         var $spanOutput2 = $(this).text();
+  //         right = right + $spanOutput2;
+  //         console.log(right);
+  //       });
+  //   });
