6ab316a40ab71fac02debbad007e6955f0b2fba8
Jenni Bradstreet <jennic15@gmail.com>
2016-03-06 13:26:50 -0800

    Edit code to properly display inputs

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 
-
   $(document).ready(function() {
     var input = '';
     var operator = '';
+    var screenOutput = $('#screen');
 
 
     $('span').click(function(event) {
-      var $spanOutput = $(this).text();
-      input = input + $spanOutput;
+      var spanOutput = $(this).text();
+      input = input + spanOutput;
       console.log(input);
+      screenOutput.text(input);
 
-      var $screenOutput = $('#screen');
-      $screenOutput.text(input);
 
-      if (input === 'x') {
-        operator = 'x';
-      } else {
-        operator = 'x';
-      }
+    });
 
+    $('.operator:not(#calc)').click(function(event) {
+      operator = $(this).text();
+      console.log(operator);
     });
 
     $('#calc').click(function(event) {
       var doTheMath = input.split(operator);
+      if ( operator === 'x') {
         console.log(parseInt(doTheMath[0]) * parseInt(doTheMath[1]));
+        screenOutput.text(parseInt(doTheMath[0]) * parseInt(doTheMath[1]));
+      } else {
+        console.log('not done');
+      }
+
     });
 
     // need to hide the "C" somehow...
     // also need to make this work with one click
 
     $('#cancel').click(function(event) {
-      input = '';
+      input = '0';
     });
 
   });
@@ -62,4 +66,47 @@
   //         right = right + $spanOutput2;
   //         console.log(right);
   //       });
+  //   });
+
+
+  // talked to Chris. Issue with spanOutput. Need to specify operator being clicked.
+  // $(document).ready(function() {
+  //   var input = '';
+  //   var operator = '';
+  //
+  //
+  //   $('span').click(function(event) {
+  //     var spanOutput = $(this).text();
+  //     input = input + spanOutput;
+  //     console.log(input);
+  //
+  //     var $screenOutput = $('#screen');
+  //     $screenOutput.text(input);
+  //
+  //     console.log(spanOutput);
+  //     // input = '12x'
+  //     if (spanOutput === 'x') {
+  //       operator = 'x';
+  //     } else if (spanOutput === '+'){
+  //       operator = '+';
+  //     } else {
+  //       operator = 'not done'
+  //     }
+  //
+  //   });
+  //
+  //   $('#calc').click(function(event) {
+  //     console.log(operator);
+  //     var doTheMath = input.split(operator);
+  //
+  //     if (operator === 'x') {
+  //       console.log(parseInt(doTheMath[0]) * parseInt(doTheMath[1]));
+  //
+  //     } else if (operator === '+') {
+  //       console.log(parseInt(doTheMath[0]) + parseInt(doTheMath[1]));
+  //
+  //     } else {
+  //       console.log('needs work');
+  //     }
+  //
   //   });
